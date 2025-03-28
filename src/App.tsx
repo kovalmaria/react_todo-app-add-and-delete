a2f07ece08bac4af86dd6ac3cd06d8e5637fc774
Mariia Koval <m.kovalmaria@gmail.com>
2025-03-29 01:51:00 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,215 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import * as todoService from './api/httpClient';
+import { ErrorMessages, FilterStatus, Todo } from './types/Todo';
+import { Header } from './component/Header';
+import { Footer } from './component/Footer';
+import { ErrorNotification } from './component/ErrorNotification';
+import { TodoList } from './component/TodoList';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { TempTodo } from './component/TempTodo';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<ErrorMessages>(
+    ErrorMessages.DEFAULT,
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [filterStatus, setFilterStatus] = useState<FilterStatus>(
+    FilterStatus.ALL,
+  );
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [isLoadingTodo, setIsLoadingTodo] = useState<number[]>([]);
+
+  function loadTodos() {
+    setIsLoading(true);
+
+    todoService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage(ErrorMessages.LOAD_TODOS);
+        setTimeout(() => {
+          setErrorMessage(ErrorMessages.DEFAULT);
+        }, 3000);
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(loadTodos, []);
+
+  const filteredTodos = todos.filter(todo => {
+    switch (filterStatus) {
+      case FilterStatus.ACTIVE:
+        return !todo.completed;
+      case FilterStatus.COMPLETED:
+        return todo.completed;
+      default:
+        return true;
+    }
+  });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleAddTodo = ({ id, title, completed, userId }: Todo) => {
+    setTempTodo({ id, title, completed, userId });
+    (inputRef.current as HTMLInputElement).disabled = true;
+
+    todoService
+      .createTodo({ title, completed, userId })
+      .then(newTodo => {
+        setTodos(currentTodos => [...currentTodos, newTodo]);
+        setTempTodo(null);
+        setQuery('');
+        (inputRef.current as HTMLInputElement).disabled = false;
+        inputRef.current?.focus();
+      })
+      .catch(() => {
+        setErrorMessage(ErrorMessages.ADD_TODO);
+        setTimeout(() => {
+          setErrorMessage(ErrorMessages.DEFAULT);
+        }, 3000);
+        setTempTodo(null);
+        (inputRef.current as HTMLInputElement).disabled = false;
+        inputRef.current?.focus();
+      });
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    setIsLoadingTodo(prev => [...prev, todoId]);
+    todoService
+      .deleteTodo(todoId)
+      .then(() => {
+        const selectedTodo = todos.filter(todo => todo.id !== todoId);
+
+        setTodos(selectedTodo);
+        inputRef.current?.focus();
+      })
+      .catch(() => {
+        setErrorMessage(ErrorMessages.DELETE_TODO);
+        setTimeout(() => {
+          setErrorMessage(ErrorMessages.DEFAULT);
+        }, 3000);
+        inputRef.current?.focus();
+      })
+      .finally(() =>
+        setIsLoadingTodo(prev => prev.filter(id => id !== todoId)),
+      );
+  };
+
+  const handleDeleteAllCompletedTodos = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    if (completedTodos.length === 0) {
+      return;
+    }
+
+    setIsLoadingTodo(completedTodos.map(todo => todo.id));
+
+    completedTodos.forEach(todo =>
+      todoService
+        .deleteTodo(todo.id)
+        .then(() => {
+          setTodos(currentTodos =>
+            currentTodos.filter(el => el.id !== todo.id),
+          );
+          inputRef.current?.focus();
+        })
+        .catch(() => {
+          setErrorMessage(ErrorMessages.DELETE_TODO);
+          setTimeout(() => {
+            setErrorMessage(ErrorMessages.DEFAULT);
+          }, 3000);
+          inputRef.current?.focus();
+        })
+        .finally(() => {
+          setIsLoadingTodo([]);
+        }),
+    );
+  };
+
+  const handleUpdateTodo = (updatedTodo: Todo) => {
+    setIsLoadingTodo([...isLoadingTodo, updatedTodo.id]);
+    todoService
+      .updateTodo(updatedTodo)
+      .then(todo => {
+        setTodos(currentTodos => {
+          return currentTodos.map(item => (todo.id === item.id ? todo : item));
+        });
+      })
+      .catch(() => {
+        setIsLoadingTodo(isLoadingTodo.filter(id => id !== updatedTodo.id));
+        setErrorMessage(ErrorMessages.UPDATE_TODO);
+        setTimeout(() => {
+          setErrorMessage(ErrorMessages.DEFAULT);
+        }, 3000);
+      })
+      .finally(() => {
+        setIsLoadingTodo(ids => ids.filter(id => id !== updatedTodo.id));
+      });
+  };
+
+  const handleChangeCompletedAllTodos = () => {
+    const areAllCompleted = todos.every(todo => todo.completed);
+
+    setTodos(
+      todos.map(todo => ({
+        ...todo,
+        completed: !areAllCompleted,
+      })),
+    );
+  };
+
+  if (!todoService.USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          handleChangeCompletedAllTodos={handleChangeCompletedAllTodos}
+          handleAddTodo={handleAddTodo}
+          setErrorMessage={setErrorMessage}
+          query={query}
+          setQuery={setQuery}
+          inputRef={inputRef}
+        />
+
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <TodoList
+              filteredTodos={filteredTodos}
+              handleDeleteTodo={handleDeleteTodo}
+              isLoadingTodo={isLoadingTodo}
+              handleUpdateTodo={handleUpdateTodo}
+            />
+            {tempTodo && <TempTodo tempTodo={tempTodo} />}
+          </>
+        )}
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            filterStatus={filterStatus}
+            setFilterStatus={setFilterStatus}
+            handleDeleteAllCompletedTodos={handleDeleteAllCompletedTodos}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
   );
 };
---
+++ src/component/TodoList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  filteredTodos: Todo[];
+  handleDeleteTodo: (id: number) => void;
+  isLoadingTodo: number[];
+  handleUpdateTodo: (todo: Todo) => void;
+};
+
+export const TodoList: React.FC<Props> = ({
+  filteredTodos,
+  handleDeleteTodo,
+  isLoadingTodo,
+  handleUpdateTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {filteredTodos.map(todo => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleDeleteTodo={handleDeleteTodo}
+            isLoadingTodo={isLoadingTodo}
+            handleUpdateTodo={handleUpdateTodo}
+          />
+        );
+      })}
+    </section>
+  );
+};
